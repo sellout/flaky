8cc9a6dcfa7de2ccadf660549fb0d24662d45406
Greg Pfeil <greg@technomadic.org>
2023-10-24 22:12:37 -0600

    Fix some base config
    - projects now set `commit-by-default` to `true` (Nix projects set `mkForce false`)
    - shellcheck now uses the shellcheck module instead of defining the file
    manually
    - adds a comment about a shortcoming of the current github confic

--- base/.config/project/shellcheck/default.nix
+++ base/.config/project/shellcheck/default.nix
@@ -1,10 +1,4 @@
-{config, lib, pkgs, ...}: {
-  project = {
-    file.".shellcheckrc" = {
-      ## TODO: Should be able to make this `"store"`.
-      minimum-persistence = "worktree";
-      source = ./rc;
-    };
-    packages = [pkgs.shellcheck];
-  };
+{pkgs, ...}: {
+  programs.shellcheck.settings.source = ./rc;
+  project.packages = [pkgs.shellcheck];
 }
