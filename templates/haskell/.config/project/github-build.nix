a61125c6739e61eaa71e636981f871129da9df3a
Greg Pfeil <greg@technomadic.org>
2023-11-11 23:55:03 -0700

    Correct reference to GitHub action
    A recent PR updated the name, but failed to update the version.

--- templates/haskell/.config/project/github-build.nix
+++ templates/haskell/.config/project/github-build.nix
@@ -24,7 +24,7 @@
       steps = [
         {uses = "actions/checkout@v2";}
         {
-          uses = "haskell-actions/setup@v1";
+          uses = "haskell-actions/setup@v2";
           id = "setup-haskell-cabal";
           "with" = {
             ghc-version = "${{ matrix.ghc }}";
