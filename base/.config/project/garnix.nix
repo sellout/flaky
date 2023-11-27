0f5b408645cd69c8b8fe01cd911352618c92fbb7
Greg Pfeil <greg@technomadic.org>
2023-11-26 19:31:37 -0700

    Exclude x86_64-darwin builds
    garnix runs them now, but they all fail, so disable them for now.

--- base/.config/project/garnix.nix
+++ base/.config/project/garnix.nix
@@ -1,6 +1,9 @@
 {
   services.garnix.builds = {
     exclude = [
+      ## TODO: Enable once garnix-io/issues#16 is closed.
+      "*.x86_64-darwin"
+      "*.x86_64-darwin.*"
       ## TODO: This check requires Internet access.
       "checks.*.project-manager-files"
     ];
