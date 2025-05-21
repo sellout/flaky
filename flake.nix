61a449c4cd15b9b0370d51f35a6769f41efa3c82
Greg Pfeil <greg@technomadic.org>
2025-05-21 13:47:36 -0600

    Match Home Manager version to Nixpkgs

--- flake.nix
+++ flake.nix
@@ -126,7 +126,7 @@
 
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
     };
 
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
