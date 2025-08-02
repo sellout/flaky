737fe500235f22a99dca6bc945b2744a59612732
Greg Pfeil <greg@technomadic.org>
2025-08-02 01:53:41 -0600

    Disable prettier on i686-linux

--- base/.config/project/treefmt.nix
+++ base/.config/project/treefmt.nix
@@ -6,7 +6,8 @@
       alejandra.enable = true;
       dhall.lint = true;
       ## Web/JSON/Markdown/TypeScript/YAML formatter
-      prettier.enable = true;
+      ## TODO: This hangs during compile on i686-linux with Nixpkgs 25.05.
+      prettier.enable = pkgs.system != "i686-linux";
       ## Shell formatter
       ## NB: This has to be unset to allow the .editorconfig settings to be
       ##     used. See numtide/treefmt-nix#96.
