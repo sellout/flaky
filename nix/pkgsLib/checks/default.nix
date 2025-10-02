a1169fe0011bd6b0bf4cb8d18f7d89a976581c36
Greg Pfeil <greg@technomadic.org>
2025-10-02 17:06:53 -0600

    Enable `allow-import-from-derivation`
    And document the places that induce the requirement.

--- nix/pkgsLib/checks/default.nix
+++ nix/pkgsLib/checks/default.nix
@@ -3,6 +3,9 @@
   runEmptyCommand,
   self,
 }: let
+  ## This produces a fixed-output derivation so we can sandbox a derivation that
+  ## otherwise couldn’t be. However, it uses IFD, so only use it on derivations
+  ## that wouldn’t otherwise be sandboxable.
   simple = name: src: nativeBuildInputs:
     runEmptyCommand name {inherit nativeBuildInputs src;};
 in {
