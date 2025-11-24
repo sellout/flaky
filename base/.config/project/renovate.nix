06c18021ef43a32fbedc3b966e4a2000087f9b2a
Greg Pfeil <greg@technomadic.org>
2025-11-24 16:44:57 -0700

    Add explanation for PM switch token

--- base/.config/project/renovate.nix
+++ base/.config/project/renovate.nix
@@ -41,6 +41,8 @@
           "with" = {
             repository = "\${{ github.event.pull_request.head.repo.full_name }}";
             ref = "\${{ github.event.pull_request.head.ref }}}";
+            ## This uses a custom token because with the default GITHUB_TOKEN,
+            ## it won’t re-run checks after creating the PR.
             token = "\${{ secrets.PROJECT_MANAGER_TOKEN }}";
           };
         }
