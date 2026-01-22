3f3daba75762d1f02cceb523b3520e0ec12b1617
Greg Pfeil <greg@technomadic.org>
2026-01-22 10:41:59 -0700

    Take advantage of “base” config locally

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -6,29 +6,11 @@
     summary = "Shared configuration for dev environments";
   };
 
-  ## dependency management
-  services.renovate.enable = true;
-
-  ## development
   programs = {
-    direnv.enable = true;
-    git = {
-      # TODO: This should default by whether there is a .git file/dir (and
-      #       whether it’s a file (worktree) or dir determines other things –
-      #       like where hooks are installed.
-      enable = true;
-      ignoreRevs = [
-        "dc0697c51a4ed5479d3ac7fcb304478729ab2793" # nix fmt
-      ];
-    };
-  };
-
-  ## formatting
-  editorconfig.enable = true;
-  programs = {
-    treefmt.enable = true;
+    git.ignoreRevs = [
+      "dc0697c51a4ed5479d3ac7fcb304478729ab2793" # nix fmt
+    ];
     vale = {
-      enable = true;
       ## This is a personal repository.
       formatSettings."*"."Microsoft.FirstPerson" = "NO";
       vocab.${config.project.name}.accept = [
@@ -45,12 +27,10 @@
     };
   };
 
-  ## publishing
-  services.flakehub.enable = true;
-  services.flakestry.enable = true;
-  services.github.enable = true;
-  services.github.settings.repository.topics = [
-    "development"
-    "nix-flakes"
-  ];
+  services = {
+    github.settings.repository.topics = [
+      "development"
+      "nix-flakes"
+    ];
+  };
 }
