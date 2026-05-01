83ff865e71a3474f91559901645766157bdba14c
Greg Pfeil <greg@technomadic.org>
2026-05-01 14:22:16 -0600

    Read .envrc files in ancestor directories
    This is helpful with Astrolabe, which uses Direnv to set up references between
    repositories in a task.

--- base/.config/project/direnv.nix
+++ base/.config/project/direnv.nix
@@ -2,6 +2,9 @@
   programs.direnv = {
     auto-allow = true;
     envrc = ''
+      ## This gives us cascading behavior. See
+      ## https://github.com/direnv/direnv/blob/master/man/direnv-stdlib.1.md#source_up_if_exists-filename
+      source_up_if_exists
       use flake
     '';
   };
